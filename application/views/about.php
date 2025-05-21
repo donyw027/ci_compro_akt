0c8cd28a553ccb2ba260b420d3d163ab40872c06
unknown <doniwicaksono27@gmail.com>
2025-05-21 11:35:06 +0700

    edit about

--- application/views/about.php
+++ application/views/about.php
@@ -26,25 +26,51 @@
 					<h2>About </h2>
 					<img src="<?= base_url('assets/img/section-img.png') ?>" alt="#">
 					<p style="text-align: justify;">
-						We are a manufacturing company specializing in the production of high-quality musical instruments, including guitars, ukuleles, and cajons. Since our founding in 2012, we have been committed to creating instruments that not only produce great sound, but also provide an unforgettable musical experience. <br>
+						We are a manufacturing company specializing in the production of high-quality musical instruments, including guitars, ukuleles, and cajons. We have been committed to creating instruments that not only produce great sound, but also provide an unforgettable musical experience. <br>
 
 						Using modern technology combined with traditional craftsmanship, every product we produce is a testament to our dedication to quality and beauty in every detail. We are proud to be part of the global music industry, providing musical instruments used by musicians from all over the world. <br>
 
 						<br><b>Our Brands</b> <br>
 						To meet the various needs of the market, we have three leading brands that cover a wide range of musical instruments: <br><br>
 
-						<b>Bromo</b><br>
-						Known for its exceptional quality and elegant design, Bromo creates guitars and ukuleles that are suitable for musicians of all levels, from beginners to professionals.<br><br>
-
-						<b>Valencia</b><br>
-						The Valencia brand offers guitars with clear sound and high durability. Chosen by many musicians who prioritize performance in every performance.<br><br>
-
-						<b>Mahalo</b><br>
-						Mahalo is our brand known for its high-quality ukuleles and innovative designs, perfect for musicians looking for an affordable yet quality instrument.
 					</p>
+
+					<div class="row">
+						<!-- Single Table -->
+						<div class="col-lg-4 col-md-12 col-12">
+
+							<b>Bromo</b><br><br>
+							<a class="btn btn-sm" style="color: white;" target="_blank" href="https://www.bromoguitars.com">bromoguitars.com</a><br><br>
+							<a class="btn btn-sm" style="color: white;" target="_blank" href="https://www.bromoguitars.com/contact">Bromo Distributors</a>
+
+
+						</div>
+						<div class="col-lg-4 col-md-12 col-12">
+							<b>Valencia</b><br><br>
+
+							<a class="btn btn-sm" style="color: white;" target="_blank" href="https://www.valenciaguitars.com">valenciaguitars.com</a><br><br>
+							<a class="btn btn-sm" style="color: white;" target="_blank" href="https://valenciaguitars.com/id/distributors/">Valencia Distributors</a>
+
+
+						</div>
+
+						<div class="col-lg-4 col-md-12 col-12">
+							<b>Mahalo</b><br><br>
+
+							<a class="btn btn-sm" style="color: white;" target="_blank" href="https://www.mahaloukuleles.com">mahaloukuleles.com</a><br><br>
+							<a class="btn btn-sm" style="color: white;" target="_blank" href="https://mahaloukuleles.com/distributors-2/">Mahalo Distributors</a>
+
+
+						</div>
+						<!-- End Single Table-->
+					</div>
+
+
+
+
 				</div>
 			</div>
-			
+
 		</div>
 
 	</div>
